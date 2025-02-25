3c8f2be5a5d8869083f02ef324f1a77bbdbdff08
Rober Aviles <authav.c@gmail.com>
2025-02-24 23:59:13 -0600

    Add: adding logout action

--- src/modules/auth/components/SideMenu.vue
+++ src/modules/auth/components/SideMenu.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="col-md-2 sidebar">
     <div class="text-center mb-3">
-      <img src="/src/icons/cobach-logo.png" alt="Logo COBACH 217" class="img-fluid" style="max-width: 100px;">
+      <img
+        src="/src/icons/cobach-logo.png"
+        alt="Logo COBACH 217"
+        class="img-fluid"
+        style="max-width: 100px"
+      />
     </div>
     <div class="p-1 mb-2 bg-transparent text-body"></div>
     <h5 class="sidebar-title text-center">Plantel 217</h5>
@@ -22,6 +27,24 @@
           <i class="bi bi-clock-history me-2"></i> Historial Académico
         </router-link>
       </li>
+      <!-- Botón de Logout -->
+      <li class="nav-item">
+        <button class="nav-link text-dark d-flex align-items-center" @click="logout">
+          <i class="bi bi-box-arrow-left me-2"></i> Cerrar Sesión
+        </button>
+      </li>
     </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth.store';
+
+const authStore = useAuthStore();
+
+const logout = () => {
+  authStore.clearUser();
+  // Redirigir al usuario a la página de login o a la página principal
+  window.location.href = '/login';
+};
+</script>
